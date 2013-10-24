1732cf2a9b44427b5c3b4db4765fbff7bb957358
Owen <owen.parrish@gmail.com>
2013-10-24 10:22:30 -0400

    Include content metadata

--- s3_podcast.rb
+++ s3_podcast.rb
@@ -75,6 +75,7 @@ content = RSS::Maker.make(version) do |m|
 		i.enclosure.url = i.link
 		i.enclosure.length = audio.content_length
 		i.enclosure.type = audio.content_type
+		i.encoded :content audio.metadata[:content]
   end 
 end
 
